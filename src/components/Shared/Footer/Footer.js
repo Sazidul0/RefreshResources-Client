7365b64ba1c5ef50df5e1e9ab3a1450e1f64ce23
rejaul-islam1 <rejaul.hero@gmail.com>
2022-05-01 01:13:46 +0600

    footer added

--- src/components/Shared/Footer/Footer.js
+++ src/components/Shared/Footer/Footer.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
 const Footer = () => {
+    const today = new Date();
+    const year = today.getFullYear();
     return (
-        <div>
-            <p><small>Copyright 2022</small></p>
+        <div className='bg-dark text-white mt-5 px-3 py-4 text-center'>
+            <h2 className=''>Refresh Resources</h2>
+            <p className='mb-0'><small>Copyright © {year}</small></p>
         </div>
     );
 };
